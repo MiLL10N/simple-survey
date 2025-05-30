4aa3ab7465fe90feca5ac396b4e44f8e1dcbde4f
Kerdkaw Sonza <novemixth@gmail.com>
2025-05-30 15:27:01 +0700

    Change page title and remove some code comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google"; // Import Poppins
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
-  subsets: ["latin"], // Add 'thai' subset
-  variable: "--font-poppins", // Define a CSS variable
-  weight: ["300", "400", "500", "600", "700", "800"], // Specify desired weights
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
-  title: "Simple Questionnaire", // Updated title
-  description: "A user-friendly questionnaire application.", // Updated description
+  title: "Simple Survey",
+  description: "A user-friendly customizable survey application.", // Updated description
 };
 
 export default function RootLayout({
